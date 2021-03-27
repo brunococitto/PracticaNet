97656fd705982b9b742bfb32765e3c1d3a042a32
Bruno Cocitto <bcocitto@gmail.com>
2021-03-27 11:39:45 -0300

    Prueba App Consola con Net 5
    Prueba App Consola con Net 5

---
+++ PruebaNet5Consola/PruebaNet5Consola/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PruebaNet5Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Prueba app consola con Net 5");
+            Console.ReadKey();
+        }
+    }
+}
