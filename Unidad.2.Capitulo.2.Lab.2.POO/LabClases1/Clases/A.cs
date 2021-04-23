a5971ec8bdab75622d55ff0ea033e462c70a8a71
Bruno Cocitto <bcocitto@gmail.com>
2021-04-23 17:59:15 -0300

    unidad 2 capitulo 2 lab 2 POO - hechos ejercicios 1 y 2

---
+++ Unidad.2.Capitulo.2.Lab.2.POO/LabClases1/Clases/A.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Clases
+{
+    public class A
+    {
+        private string _NombreInstancia;
+        public A ()
+        {
+            _NombreInstancia = "Instancia sin nombre";
+        }
+        public A(string nombre)
+        {
+            _NombreInstancia = nombre;
+        }
+        // se puede hacer como public string NombreInstancia { get; set; };
+        public string NombreInstancia
+        {
+            get
+            {
+                return _NombreInstancia;
+            }
+            set
+            {
+                _NombreInstancia = value;
+            }
+        }
+        public void MostrarNombre()
+        {
+            Console.WriteLine(_NombreInstancia);
+        }
+        public void M1()
+        {
+            Console.WriteLine("El método M1 fue invocado.");
+        }
+        public void M2()
+        {
+            Console.WriteLine("El método M2 fue invocado.");
+        }
+        public void M3()
+        {
+            Console.WriteLine("El método M2 fue invocado.");
+        }
+    }
+}
---
+++ Unidad.2.Capitulo.2.Lab.2.POO/LabClases1/LabClases1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Clases;
+
+namespace LabClases1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A objeto_a_por_defecto = new A();
+            A objeto_a_con_nombre = new A("Este a instancia sí tiene nombre");
+            B objeto_b = new B();
+            objeto_a_por_defecto.MostrarNombre();
+            objeto_a_por_defecto.M1();
+            objeto_a_por_defecto.M2();
+            objeto_a_por_defecto.M3();
+            objeto_a_con_nombre.MostrarNombre();
+            objeto_b.MostrarNombre();
+            objeto_b.M1();
+            objeto_b.M4();
+        }
+    }
+}
