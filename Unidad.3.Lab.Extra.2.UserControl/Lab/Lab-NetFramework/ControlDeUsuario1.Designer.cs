3e6c60f4ecb82f990647c9b588bc78b2633a8fea
Bruno Cocitto <bcocitto@gmail.com>
2021-05-21 12:42:33 -0300

    Hecho Unidad 3 Lab Extra 2 - User Control
    Lo hice con Net Framework porque con Net 5 no es compatible la suite Krypton que usa el lab

---
+++ Unidad.3.Lab.Extra.2.UserControl/Lab/Lab-NetFramework/ControlDeUsuario1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace Lab_NetFramework
+{
+    partial class ControlDeUsuario1
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ktnTxtNombreApellido = new ComponentFactory.Krypton.Toolkit.KryptonTextBox();
+            this.ktnTxtDireccion = new ComponentFactory.Krypton.Toolkit.KryptonTextBox();
+            this.ktnTxtTelefono = new ComponentFactory.Krypton.Toolkit.KryptonTextBox();
+            this.ktnTxtEmail = new ComponentFactory.Krypton.Toolkit.KryptonTextBox();
+            this.ktnHeader = new ComponentFactory.Krypton.Toolkit.KryptonHeader();
+            this.kryptonLabel1 = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
+            this.kryptonLabel2 = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
+            this.kryptonLabel3 = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
+            this.kryptonLabel4 = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
+            this.ktnBtnGuardar = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.ktnBtnCancelar = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.kryptonManager1 = new ComponentFactory.Krypton.Toolkit.KryptonManager(this.components);
+            this.SuspendLayout();
+            // 
+            // ktnTxtNombreApellido
+            // 
+            this.ktnTxtNombreApellido.Location = new System.Drawing.Point(161, 39);
+            this.ktnTxtNombreApellido.Name = "ktnTxtNombreApellido";
+            this.ktnTxtNombreApellido.Size = new System.Drawing.Size(143, 23);
+            this.ktnTxtNombreApellido.TabIndex = 0;
+            // 
+            // ktnTxtDireccion
+            // 
+            this.ktnTxtDireccion.Location = new System.Drawing.Point(161, 68);
+            this.ktnTxtDireccion.Name = "ktnTxtDireccion";
+            this.ktnTxtDireccion.Size = new System.Drawing.Size(143, 23);
+            this.ktnTxtDireccion.TabIndex = 1;
+            // 
+            // ktnTxtTelefono
+            // 
+            this.ktnTxtTelefono.Location = new System.Drawing.Point(161, 97);
+            this.ktnTxtTelefono.Name = "ktnTxtTelefono";
+            this.ktnTxtTelefono.Size = new System.Drawing.Size(143, 23);
+            this.ktnTxtTelefono.TabIndex = 2;
+            // 
+            // ktnTxtEmail
+            // 
+            this.ktnTxtEmail.Location = new System.Drawing.Point(161, 126);
+            this.ktnTxtEmail.Name = "ktnTxtEmail";
+            this.ktnTxtEmail.Size = new System.Drawing.Size(143, 23);
+            this.ktnTxtEmail.TabIndex = 3;
+            // 
+            // ktnHeader
+            // 
+            this.ktnHeader.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ktnHeader.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowOnly;
+            this.ktnHeader.Location = new System.Drawing.Point(0, 0);
+            this.ktnHeader.Name = "ktnHeader";
+            this.ktnHeader.Size = new System.Drawing.Size(317, 33);
+            this.ktnHeader.TabIndex = 4;
+            this.ktnHeader.Values.Description = "";
+            this.ktnHeader.Values.Heading = "Nuevo Cliente";
+            // 
+            // kryptonLabel1
+            // 
+            this.kryptonLabel1.Location = new System.Drawing.Point(39, 39);
+            this.kryptonLabel1.Name = "kryptonLabel1";
+            this.kryptonLabel1.Size = new System.Drawing.Size(116, 20);
+            this.kryptonLabel1.TabIndex = 5;
+            this.kryptonLabel1.Values.Text = "Nombre y Apellido:";
+            // 
+            // kryptonLabel2
+            // 
+            this.kryptonLabel2.Location = new System.Drawing.Point(88, 71);
+            this.kryptonLabel2.Name = "kryptonLabel2";
+            this.kryptonLabel2.Size = new System.Drawing.Size(64, 20);
+            this.kryptonLabel2.TabIndex = 6;
+            this.kryptonLabel2.Values.Text = "Dirección:";
+            // 
+            // kryptonLabel3
+            // 
+            this.kryptonLabel3.Location = new System.Drawing.Point(91, 97);
+            this.kryptonLabel3.Name = "kryptonLabel3";
+            this.kryptonLabel3.Size = new System.Drawing.Size(61, 20);
+            this.kryptonLabel3.TabIndex = 7;
+            this.kryptonLabel3.Values.Text = "Teléfono:";
+            // 
+            // kryptonLabel4
+            // 
+            this.kryptonLabel4.Location = new System.Drawing.Point(104, 129);
+            this.kryptonLabel4.Name = "kryptonLabel4";
+            this.kryptonLabel4.Size = new System.Drawing.Size(48, 20);
+            this.kryptonLabel4.TabIndex = 8;
+            this.kryptonLabel4.Values.Text = "E-Mail:";
+            // 
+            // ktnBtnGuardar
+            // 
+            this.ktnBtnGuardar.Location = new System.Drawing.Point(62, 172);
+            this.ktnBtnGuardar.Name = "ktnBtnGuardar";
+            this.ktnBtnGuardar.Size = new System.Drawing.Size(90, 25);
+            this.ktnBtnGuardar.TabIndex = 9;
+            this.ktnBtnGuardar.Values.Text = "Guardar";
+            // 
+            // ktnBtnCancelar
+            // 
+            this.ktnBtnCancelar.Location = new System.Drawing.Point(161, 172);
+            this.ktnBtnCancelar.Name = "ktnBtnCancelar";
+            this.ktnBtnCancelar.Size = new System.Drawing.Size(90, 25);
+            this.ktnBtnCancelar.TabIndex = 10;
+            this.ktnBtnCancelar.Values.Text = "Cancelar";
+            // 
+            // kryptonManager1
+            // 
+            this.kryptonManager1.GlobalPaletteMode = ComponentFactory.Krypton.Toolkit.PaletteModeManager.SparkleBlue;
+            // 
+            // ControlDeUsuario1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.ktnBtnCancelar);
+            this.Controls.Add(this.ktnBtnGuardar);
+            this.Controls.Add(this.kryptonLabel4);
+            this.Controls.Add(this.kryptonLabel3);
+            this.Controls.Add(this.kryptonLabel2);
+            this.Controls.Add(this.kryptonLabel1);
+            this.Controls.Add(this.ktnHeader);
+            this.Controls.Add(this.ktnTxtEmail);
+            this.Controls.Add(this.ktnTxtTelefono);
+            this.Controls.Add(this.ktnTxtDireccion);
+            this.Controls.Add(this.ktnTxtNombreApellido);
+            this.Name = "ControlDeUsuario1";
+            this.Size = new System.Drawing.Size(317, 211);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ComponentFactory.Krypton.Toolkit.KryptonTextBox ktnTxtNombreApellido;
+        private ComponentFactory.Krypton.Toolkit.KryptonTextBox ktnTxtDireccion;
+        private ComponentFactory.Krypton.Toolkit.KryptonTextBox ktnTxtTelefono;
+        private ComponentFactory.Krypton.Toolkit.KryptonTextBox ktnTxtEmail;
+        private ComponentFactory.Krypton.Toolkit.KryptonHeader ktnHeader;
+        private ComponentFactory.Krypton.Toolkit.KryptonLabel kryptonLabel1;
+        private ComponentFactory.Krypton.Toolkit.KryptonLabel kryptonLabel2;
+        private ComponentFactory.Krypton.Toolkit.KryptonLabel kryptonLabel3;
+        private ComponentFactory.Krypton.Toolkit.KryptonLabel kryptonLabel4;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton ktnBtnGuardar;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton ktnBtnCancelar;
+        private ComponentFactory.Krypton.Toolkit.KryptonManager kryptonManager1;
+    }
+}
