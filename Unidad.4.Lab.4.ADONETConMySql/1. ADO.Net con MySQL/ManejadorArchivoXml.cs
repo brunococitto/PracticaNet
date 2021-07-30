9b8aa5cb9ad1c09e012b7c492dbe41625dc886cc
Bruno Cocitto <bcocitto@gmail.com>
2021-07-30 17:56:54 -0300

    Realizado Unidad 4 Lab 4 ADO.NET con MySQL

---
+++ Unidad.4.Lab.4.ADONETConMySql/1. ADO.Net con MySQL/ManejadorArchivoXml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace _1._ADO.Net_con_MySQL
+{
+    class ManejadorArchivoXml : ManejadorArchivo
+    {
+        protected DataSet ds;
+        public override System.Data.DataTable getTabla()
+        {
+            this.ds = new DataSet();
+            this.ds.ReadXml("agenda.xml");
+            return this.ds.Tables["contactos"];
+        }
+        public override void aplicaCambios()
+        {
+            this.ds.WriteXml("agenda.xml");
+            this.ds.WriteXml("agendaconschemaxml", XmlWriteMode.WriteSchema);
+        }
+    }
+}
